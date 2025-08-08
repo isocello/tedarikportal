4e51f1a4cb25dbec43119a006aaaec1f216e0dfd
isocello <ihsan.celik.uk@gmail.com>
2025-08-08 15:12:44 +0300

    Create AdminPanel.jsx

---
+++ src/pages/AdminPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminPanel() {
+  return (
+    <div>
+      <h2>Admin Panel</h2>
+      <p>This will let you add tenders and view bids (not implemented yet).</p>
+    </div>
+  )
+}
