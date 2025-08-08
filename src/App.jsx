9091741223d95aff8a0b65bed3a1b0b09b06b907
isocello <ihsan.celik.uk@gmail.com>
2025-08-08 15:09:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import TenderList from './pages/TenderList'
+import BidForm from './pages/BidForm'
+import AdminPanel from './pages/AdminPanel'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/tenders" element={<TenderList />} />
+      <Route path="/bid/:tenderId" element={<BidForm />} />
+      <Route path="/admin" element={<AdminPanel />} />
     </Routes>
   )
 }
