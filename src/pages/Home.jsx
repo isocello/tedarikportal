a4284d9ab286c3fb58121e84e81815cf7552e8c4
isocello <ihsan.celik.uk@gmail.com>
2025-08-08 15:13:30 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  return <h1>Welcome to the Bidding Portal</h1>
+  return (
+    <div>
+      <h1>Welcome to the Bidding Portal</h1>
+      <nav>
+        <Link to="/tenders">View Tenders</Link> |{' '}
+        <Link to="/admin">Admin Panel</Link>
+      </nav>
+    </div>
+  )
 }
